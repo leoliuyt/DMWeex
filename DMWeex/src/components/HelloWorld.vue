4c2e1d3de1be5cb4eb55a47731d0b0c23317422a
leoliu <leoliu@leoliudeMacBook-Pro.local>
2018-11-11 17:13:21 +0800

    create full weex project

---
+++ DMWeex/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <text class="message1">Now, let's use Vue.js to build your Weex app.</text>
+  <text class="message1">Now, let's use Vue.js to build your Weex app.</text>
+  <text class="message1">Now, let's use Vue.js to build your Weex app.</text>
+</div>
+</template>
+
+<style scoped>
+.message1{
+  color: #FF0000
+}
+</style>
